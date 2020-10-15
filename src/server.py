968ba4f4b0eb8b42a3c87c196690bab1c56c5ae4
Christoph Ostrzinski <christoph@ostrzinski.de>
2020-10-15 17:48:47 +0200

    added json init

--- src/server.py
+++ src/server.py
@@ -1,6 +1,44 @@
 from flask import Flask
 app = Flask(__name__)
 
+from Booking import Booking
+from BookingList import BookingList
+from Car import Car
+from Customer import Customer
+
+path = "../resouces/car_data.json"
+
+customers = []
+booking_list = BookingList()
+cars = []
+
+def load_json():
+    with open(path) as data:
+        data = json.load(data)
+        cars_import = data["data"]["cars"]
+        customers_import = data["customers"]
+
+        for car in cars_import:
+            new_car = Car(car["name"], car["color"], car["number_of_seats"], car["brand"])
+            cars.append(new_car)
+
+        for customer in customers_import:
+            new_Customer = Customer(customer["first_name"], customer["last_name"])
+            customers.append(new_Customer)
+            car_assigned = False
+            while not car_assigned:
+                car = cars[random.randint(0, len(cars) - 1)]
+                car_assigned = booking_list.addBooking(
+                    Booking(car, new_Customer, now, now + datetime.timedelta(random.randint(1, 100))))
+
+    for car in cars:
+        print(car.to_json())
+    for customer in customers:
+        print(customer.to_json())
+    for booking in booking_list.bookings:
+        print(booking.to_json())
+
+
 #Larissa
 @app.route('/customers')
 def get_all_customers():
@@ -12,12 +50,16 @@ def get_all_cars():
     return 'Hello, World!'
 
 
-@app.route('/customer/<id>/book/<car_id>', methods=["POST"])
-def book_car(id, car_id):
+@app.route('/customer/<first_name>/<last_name>/book/<car_id>', methods=["POST"])
+def book_car(first_name, last_name, car_id):
+    for customer in customers:
+        if customer["first_name"] == first_name and customer["last_name"]:
+             
+
     return 'Hello, World!'
 
-@app.route('/customer/<id>/history')
-def get_history(id):
+@app.route('/customer/<first_name>/<last_name>/history')
+def get_history(first_name, last_name):
     return 'Hello, World!'
 
 if __name__ == '__main__':
@@ -25,6 +67,7 @@ if __name__ == '__main__':
     for i in range(100):
         try:
             app.run(host="0.0.0.0", port=8080 + i, debug=True)
+            load_json()
             break
         except OSError:
             pass
