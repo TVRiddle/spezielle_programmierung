434cd7d995a4340db706217b03222ba3e3cc43af
Matthias <s0571096@htw-berlin.de>
2020-10-19 00:09:50 +0200

    Server don't get in permanent loop

--- src/server.py
+++ src/server.py
@@ -1,3 +1,5 @@
+import copy
+
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -16,39 +18,35 @@ app = Flask(__name__)
 customers = []
 booking_list = BookingList()
 cars = []
+data_have_to_be_loaded = True
 
 now = datetime.datetime.now()
 
+
 @app.route('/init')
 def load_json():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "resources", "car_data.json")
-    with open(json_url) as data:
-        data = json.load(data)
-        cars_import = data["data"]["cars"]
-        customers_import = data["customers"]
+    if(len(cars) == 0):
+        SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+        json_url = os.path.join(SITE_ROOT, "resources", "car_data.json")
+        with open(json_url) as data:
+            data = json.load(data)
+            cars_import = data["data"]["cars"]
+            customers_import = data["customers"]
 
-        for car in cars_import:
-            new_car = Car(car["name"], car["color"], car["number_of_seats"], car["brand"])
-            cars.append(new_car)
+            for car in cars_import:
+                new_car = Car(car["name"], car["color"], car["number_of_seats"], car["brand"])
+                cars.append(new_car)
 
-        for customer in customers_import:
-            new_Customer = Customer(customer["first_name"], customer["last_name"])
-            customers.append(new_Customer)
-            car_assigned = False
-            while not car_assigned:
-                car = cars[random.randint(0, len(cars) - 1)]
-                car_assigned = booking_list.addBooking(
-                    Booking(car, new_Customer, now, now + datetime.timedelta(random.randint(1, 100))))
-
-    for car in cars:
-        print(car.to_json())
-    for customer in customers:
-        print(customer.to_json())
-    for booking in booking_list.bookings:
-        print(booking.to_json())
-
+            for customer in customers_import:
+                new_Customer = Customer(customer["first_name"], customer["last_name"])
+                customers.append(new_Customer)
+                car_assigned = False
+                while not car_assigned:
+                    car = cars[random.randint(0, len(cars) - 1)]
+                    car_assigned = booking_list.addBooking(
+                        Booking(car, new_Customer, now, now + datetime.timedelta(random.randint(1, 100))))
     return "Done"
+
 
 # Larissa
 @app.route('/customers')
@@ -56,11 +54,13 @@ def get_all_customers():
     load_json()
     return jsonify([customer.__dict__ for customer in customers])
 
+
 # Larissa
 @app.route('/cars')
 def get_all_cars():
     load_json()
     return jsonify([car.__dict__ for car in cars])
+
 
 @app.route('/customer/<first_name>/<last_name>/book/<car_id>', methods=["GET"])
 def book_car(first_name, last_name, car_id):
@@ -74,21 +74,23 @@ def book_car(first_name, last_name, car_id):
         return "Auto bereits gebucht."
     return "Auto wurde gebucht."
 
+
 @app.route('/customer/<first_name>/<last_name>/history')
 def get_history(first_name, last_name):
     load_json()
-    found_bookings = BookingList()
-    for booking in booking_list:
-        print("Hallo")
-        if (first_name == booking.customer.first_name and last_name == booking.customer.last_name):
-            found_bookings.append(booking)
-    return jsonify([booking.__dict__ for booking in found_bookings])
+    found_bookings = []
+    for booking in booking_list.bookings:
+        if first_name == booking.customer.first_name and last_name == booking.customer.last_name:
+            found_bookings.append(copy.deepcopy(booking))
+    print(dump(found_bookings))
+    return jsonify(booking.__dict__ for booking in found_bookings)
+
 
 if __name__ == '__main__':
     # Start webserver
     for i in range(100):
         try:
-            app.run(host="0.0.0.0", port=8080 + i, debug=True)
+            app.run(host="0.0.0.0", port=8080 + i, debug=True, use_reloader=False)
             break
         except OSError:
             pass
