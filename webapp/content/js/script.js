39905a2a2bc8e8179b46fdd7f798ddf7a9a4c872
Matthias <s0571096@htw-berlin.de>
2020-10-28 23:16:31 +0100

    Final: Doku erstellt und HTML Seite schöner gemacht

--- webapp/content/js/script.js
+++ webapp/content/js/script.js
@@ -1,82 +1,44 @@
-function httpGet(url) {
-    console.log("Send request to -> " + url);
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
+Element.prototype.remove = function () {
+    this.parentElement.removeChild(this);
 }
 
-function showCustomers() {
+function loadData() {
+    // Load customers
+    let customerSelect = document.getElementById("availableCustomers");
+    customerSelect.innerHTML = "";
     let result = JSON.parse(httpGet("/api/customers"));
-    let table = document.getElementById("customers");
-    table.innerHTML = "";
-    let header = getHeader("customers");
-    table.append(header);
 
     for (let i = 0; i < result.length; i++) {
         let customer = result[i];
-        let customerElement = document.createElement('tr');
-        let vorname = document.createElement('td');
-        vorname.innerHTML = customer.first_name;
-        let nachname = document.createElement('td');
-        nachname.innerHTML = customer.last_name;
-        let tdHistoryButton = document.createElement('td');
-        let historyButton = document.createElement('button');
-        historyButton.innerHTML = "show history";
-        historyButton.classList = "btn btn-info";
-        historyButton.setAttribute("data-id", customer.first_name + "," + customer.last_name);
-        historyButton.addEventListener("click", showHistory)
-        tdHistoryButton.append(historyButton);
-
-        customerElement.append(vorname);
-        customerElement.append(nachname);
-        customerElement.append(tdHistoryButton);
-
-        table.append(customerElement);
+        let option = document.createElement("option");
+        let name = customer.first_name + "," + customer.last_name;
+        option.value = name;
+        option.innerHTML = name;
+        customerSelect.append(option);
     }
-}
 
-function showHistory() {
-    // todo hide again
-    let firstName = this.getAttribute("data-id").split(",")[0];
-    let lastName = this.getAttribute("data-id").split(",")[1];
-
-    let result = JSON.parse(httpGet("/api/customer/" + firstName + "/" + lastName + "/history"));
-
-    let newTable = document.createElement("table");
-    newTable.classList = "table-dark";
-    let header = getHeader("history");
-    newTable.append(header);
+    // Load free cars
+    let carsSelect = document.getElementById("availableCars");
+    carsSelect.innerHTML = "";
+    result = JSON.parse(httpGet("/api/cars"));
     for (let i = 0; i < result.length; i++) {
-        let history = result[i];
-        console.log(history);
-        let historyElement = document.createElement('tr');
-        let carName = document.createElement('td');
-        carName.innerHTML = history.car_name;
-        let start = document.createElement('td');
-        start.innerHTML = history.start;
-        let end = document.createElement('td');
-        end.innerHTML = history.end;
-
-        historyElement.append(carName);
-        historyElement.append(start);
-        historyElement.append(end);
-
-        newTable.append(historyElement);
+        let car = result[i];
+        if (!car.is_booked) {
+            let option = document.createElement("option");
+            let name = car.name;
+            option.value = name;
+            option.innerHTML = name;
+            carsSelect.append(option);
+        }
     }
-
-    let newTableRow = document.createElement('tr');
-    let newTableData = document.createElement('td');
-    newTableData.setAttribute("colspan", "3");
-    newTableData.append(newTable)
-    newTableRow.append(newTableData);
-    this.parentElement.parentElement.parentElement.insertBefore(newTableRow, this.parentElement.parentElement.nextSibling);
 }
 
 function showCars() {
     let result = JSON.parse(httpGet("/api/cars"));
     let table = document.getElementById("cars");
-    table.innerHTML = "";
+    if (table.hasChildNodes()) {
+        table.innerHTML = "";
+    }
     let header = getHeader("cars");
     table.append(header);
 
@@ -101,6 +63,118 @@ function showCars() {
 
         table.append(carElement);
     }
+}
+
+function showCustomers() {
+    let result = JSON.parse(httpGet("/api/customers"));
+
+    let table = document.getElementById("customers");
+    if (table.hasChildNodes()) {
+        table.innerHTML = "";
+    }
+    let header = getHeader("customers");
+    table.append(header);
+
+    for (let i = 0; i < result.length; i++) {
+        let customer = result[i];
+        let customerElement = document.createElement('tr');
+        let vorname = document.createElement('td');
+        vorname.innerHTML = customer.first_name;
+        let nachname = document.createElement('td');
+        nachname.innerHTML = customer.last_name;
+        let tdHistoryButton = document.createElement('td');
+        let historyButton = document.createElement('button');
+        historyButton.innerHTML = "show history";
+        historyButton.classList = "btn btn-info";
+        historyButton.setAttribute("data-id", customer.first_name + "," + customer.last_name);
+        historyButton.addEventListener("click", toggleHistory)
+        historyButton.tagName = "historyButton"
+        tdHistoryButton.append(historyButton);
+
+        customerElement.append(vorname);
+        customerElement.append(nachname);
+        customerElement.append(tdHistoryButton);
+
+        table.append(customerElement);
+    }
+}
+
+function toggleHistory() {
+
+    if (this.innerHTML == "hide history") {
+        hideHistory(this);
+        this.innerHTML = "show history";
+    } else {
+        showHistory(this);
+        this.innerHTML = "hide history";
+    }
+
+    function hideHistory(button) {
+        let customer = button.parentElement.parentElement;
+        let index = getIndexOfElement(customer) + 1;
+        let toRemove = customer.parentElement.childNodes.item(index);
+        customer.parentElement.removeChild(toRemove);
+    }
+
+    function showHistory(button) {
+        let firstName = button.getAttribute("data-id").split(",")[0];
+        let lastName = button.getAttribute("data-id").split(",")[1];
+
+        let result = JSON.parse(httpGet("/api/customer/" + firstName + "/" + lastName + "/history"));
+
+        let newTable = document.createElement("table");
+        newTable.classList = "table-dark";
+        let header = getHeader("history");
+        newTable.append(header);
+        for (let i = 0; i < result.length; i++) {
+            let history = result[i];
+            let historyElement = document.createElement('tr');
+            let carName = document.createElement('td');
+            carName.innerHTML = history.car_name;
+            let start = document.createElement('td');
+            start.innerHTML = history.start;
+            let end = document.createElement('td');
+            end.innerHTML = history.end;
+
+            historyElement.append(carName);
+            historyElement.append(start);
+            historyElement.append(end);
+
+            newTable.append(historyElement);
+        }
+
+        let newTableRow = document.createElement('tr');
+        let newTableData = document.createElement('td');
+        newTableData.id = "historyTable";
+        newTableData.setAttribute("colspan", "3");
+        newTableData.append(newTable)
+        newTableRow.append(newTableData);
+        button.parentElement.parentElement.parentElement.insertBefore(newTableRow, button.parentElement.parentElement.nextSibling);
+    }
+}
+
+function book() {
+    let customer = document.getElementById("availableCustomers").value;
+    let firstName = customer.split(",")[0];
+    let lastName = customer.split(",")[1];
+    let car = document.getElementById("availableCars").value;
+
+    let result = httpGet("/api/customer/" + firstName + "/" + lastName + "/book/" + car);
+
+    if (result == "true") {
+        alert("Juhu Buchung vorgenommen ;)");
+    } else {
+        alert("Que pena eres tu! Ahora se te jaman Carlos");
+    }
+    loadData();
+}
+
+function httpGet(url) {
+    console.log("Send request to -> " + url);
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
 }
 
 function getHeader(kind) {
@@ -149,49 +223,13 @@ function getHeader(kind) {
     return header;
 }
 
-function book() {
-    let customer = document.getElementById("availableCustomers").value;
-    let firstName = customer.split(",")[0];
-    let lastName = customer.split(",")[1];
-    let car = document.getElementById("availableCars").value;
-
-    let result = httpGet("/api/customer/" + firstName + "/" + lastName + "/book/" + car);
-
-    if (result=="true") {
-        alert("Juhu Buchung vorgenommen ;)");
-    } else {
-        alert("Que pena eres tu! Ahora se te jaman Carlos");
-    }
-    loadData();
-}
-
-function loadData() {
-    // Load customers
-    let customerSelect = document.getElementById("availableCustomers");
-    customerSelect.innerHTML = "";
-    let result = JSON.parse(httpGet("/api/customers"));
-
-    for (let i = 0; i < result.length; i++) {
-        let customer = result[i];
-        let option = document.createElement("option");
-        let name = customer.first_name + "," + customer.last_name;
-        option.value = name;
-        option.innerHTML = name;
-        customerSelect.append(option);
-    }
-
-    // Load free cars
-    let carsSelect = document.getElementById("availableCars");
-    carsSelect.innerHTML = "";
-    result = JSON.parse(httpGet("/api/cars"));
-    for (let i = 0; i < result.length; i++) {
-        let car = result[i];
-        if (!car.is_booked) {
-            let option = document.createElement("option");
-            let name = car.name;
-            option.value = name;
-            option.innerHTML = name;
-            carsSelect.append(option);
+function getIndexOfElement(el) {
+    var children = el.parentNode.childNodes,
+        i = 0;
+    for (; i < children.length; i++) {
+        if (children[i] == el) {
+            return i;
         }
     }
+    return -1;
 }
